768aaf209a77dcd3a8d1a0bb5dac9fa0f4913443
castromarcos9378-sys <castromarcos9378@gmail.com>
2026-09-24 07:20:05 -0400

    Añadir service worker Atalía

---
+++ atalia/sw.js
@@ -0,0 +1,5 @@
+const CACHE='atalia-registro-v1-20260924';
+const CORE=['./','./index.html','./manifest.webmanifest','./atalia-icon.svg','./sw.js'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{const r=e.request;if(r.method!=='GET')return;const u=new URL(r.url);if(u.origin!==self.location.origin)return;if(r.mode==='navigate'){e.respondWith(fetch(r).then(res=>{const cp=res.clone();caches.open(CACHE).then(c=>c.put('./index.html',cp)).catch(()=>{});return res;}).catch(()=>caches.match('./index.html')));return}e.respondWith(caches.match(r).then(v=>v||fetch(r).then(res=>{if(res&&res.ok){const cp=res.clone();caches.open(CACHE).then(x=>x.put(r,cp)).catch(()=>{})}return res})))});

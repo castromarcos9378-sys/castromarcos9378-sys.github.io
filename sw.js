6c0f812b4ba09ed4d41d457e0b1784cabe505874
castromarcos9378-sys <castromarcos9378@gmail.com>
2026-09-24 06:49:59 -0400

    DAOS V10.1: renovar caché PWA

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='daos-registro-v10-final-2';
+const CACHE='daos-registro-v10-1-pendientes-20260924';
 const CORE=['./','./index.html','./manifest.webmanifest','./daos-icon.svg','./sw.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
